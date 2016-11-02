02c2910d1e32d9d36aef0318a231604c75efa871
Christoph Heinig <christoph.heinig@materna.de>
2016-11-02 17:00:33 +0100

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+}, {
+    id: 3,
+    description: 'Bring son to scouts',
+    completed: true
+}];
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
+});
+
+// GET /todos
+app.get('/todos', function (req, res) {
+     res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var item;
+
+    todos.forEach(function (todoItem) {
+         if (todoItem.id === todoId){
+             item = todoItem;
+         }
+    });
+
+    if (item) {
+        res.json(item);
+    } else {
+        res.status(404).send();
+    }
 });
 
 app.listen(PORT, function () {
