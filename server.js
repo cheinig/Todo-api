b680ce3661339217525d813628bdba4fd60d8561
Christoph Heinig <christoph.heinig@materna.de>
2016-11-10 15:22:44 +0100

    Add ?completed filter

--- server.js
+++ server.js
@@ -13,9 +13,22 @@ app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-     res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if (typeof (queryParams.completed) !== 'undefined' && queryParams.completed.toLocaleLowerCase() === 'true'){
+        filteredTodos = _.where(filteredTodos, {
+            completed: true
+        });
+    } else if (typeof (queryParams.completed) !== 'undefined' && queryParams.completed.toLocaleLowerCase() === 'false'){
+        filteredTodos = _.where(filteredTodos, {
+            completed: false
+        });
+    }
+
+    res.json(filteredTodos);
 });
 
 // GET /todos/:id
